a733e74b7d92cdc02a570602578fbe1b6dbfc61d
Saroj <saroj@proshore.eu>
2017-04-15 14:10:50 +0545

    Added espeak.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import os
+
 from slackbot import SlackBot
 from handler import CommandHandler
 
 def main():
     print('Starting slack bot')
+    os.system("espeak 'Starting slack bot' 2>>/dev/null")
     try:
         handler = CommandHandler()
         lta_slack = SlackBot(handler)
@@ -10,9 +13,12 @@ def main():
         lta_slack.join()
     except Exception as e:
         print (e.message)
+        message = 'espeak "%s" 2>>/dev/null' % e.message 
+        os.system(message)
     
 
     print('Slack bot stopped')
+    os.system("espeak 'Slack bot stopped'  2>>/dev/null")
 
 if __name__ == '__main__':
     main()
